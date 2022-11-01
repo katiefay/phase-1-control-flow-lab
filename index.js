fca1de2c4e85087ab9c1170b396d5a7595ff416e
Katie <katiefaybehrmann@gmail.com>
2022-10-31 20:08:25 -0600

    commit tree f398ebf21060dbe2e9ffd46e0ca812914a087e64

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let feet;
+let city;
+let tip;
+
+function scuberGreetingForFeet(feet){
+  // pass in desired mileage, return a message
+  if (feet <= 400){
+    return "This one is on me!";
+  }
+  else if (400 < feet && feet < 2000){
+    return "That will be twenty bucks.";
+  }
+  else if(2000 < feet && feet < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // pass in city. if NOT nyc, no go
+  return (city === "NYC" ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // passes in level of generosity and returns appropriate salutation
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return 'Bye.';
+}
 }
